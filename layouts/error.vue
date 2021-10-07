fb6ec10da81867be117b9b0df53710cb7bf8dc05
kcska_1820 <kcska_1820@kcska.onmicrosoft.com>
2021-10-07 22:59:35 +0900

    error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,22 @@
 <template>
   <v-app dark>
+    <h1 v-if="error.statusCode === 403">
+      {{ Forbidden }}
+    </h1>
     <h1 v-if="error.statusCode === 404">
       {{ pageNotFound }}
+    </h1>
+    <h1 v-if="error.statusCode === 500">
+      {{ InternalServerError }}
+    </h1>
+    <h1 v-if="error.statusCode === 502">
+      {{ BadGateway }}
+    </h1>
+    <h1 v-if="error.statusCode === 503">
+      {{ ServiceUnavailable }}
+    </h1>
+    <h1 v-if="error.statusCode === 504">
+      {{ GatewayTimeout }}
     </h1>
     <h1 v-else>
       {{ otherError }}
@@ -23,7 +38,12 @@ export default {
   },
   data () {
     return {
+      Forbidden:'403 Forbidden',
       pageNotFound: '404 Not Found',
+      InternalServerError:'500 Internal Server Error',
+      BadGateway:'502 Bad Gateway',
+      ServiceUnavailable:'503 Service Unavailable',
+      GatewayTimeout:'504 Gateway Timeout',
       otherError: 'An error occurred'
     }
   },

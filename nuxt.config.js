c5c76a9e56cc84201637daa7b8fdbd7e17714278
丸野 廉士郎 <st20183150@GHOST.KCSNet.LOCAL>
2021-10-25 14:04:27 +0900

    add firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/firebase.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components

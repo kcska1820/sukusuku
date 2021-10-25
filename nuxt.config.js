8dafa92a74f5dfcffa841d2140633cd44f82574b
丸野 廉士郎 <st20183150@GHOST.KCSNet.LOCAL>
2021-10-25 10:20:27 +0900

    dalate :firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '/plugins/firebase.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components

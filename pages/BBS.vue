36e045358e42330b89c218d3373a16f84fbb6ad7
kcska1815 <kcska_1815@kcska.onmicrosoft.com>
2021-10-28 10:27:39 +0900

    made BBS

---
+++ pages/BBS.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card color="accent">
+        <v-card-title>イベントスケジュール</v-card-title>
+        <div
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact>
+        <v-col>
+        <v-card>
+          <v-card-title>{{item.title}}</v-card-title>
+          <v-card-actions>{{item.thread}}</v-card-actions>
+        </v-card>
+        </v-col>
+        <v-divider></v-divider>
+        </div>
+    </v-card>
+</template>
+
+<script>
+  import items from '/components/topicList.json'
+  export default {
+    data: () => (
+        {items:items}
+    ),
+  }
+</script>

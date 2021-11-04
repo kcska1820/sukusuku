284b96f32bb1d009440849012654d16c8fecd618
丸野 廉士郎 <st20183150@GHOST.KCSNet.LOCAL>
2021-11-04 11:14:16 +0900

    update login

--- pages/index.vue
+++ pages/index.vue
@@ -14,11 +14,40 @@
 </template>
 
 <script>
+import firebase from "~/plugins/firebase"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 export default {
+    methods:{
+        login(){
+            const provider = new GoogleAuthProvider();
+            const auth = getAuth();
+            signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // ...
+                console.log(user,token)
+                this.$router.push("/student/calendars")
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            })
+        }
+    },
     created(){
-        setTimeout(()=>{
-            this.$router.push('/login')
-        },2*1000)
+        /* setTimeout(()=>{
+            //ログイン処理を実行
+            this.login()
+        },2*1000) */
     },
 }
 

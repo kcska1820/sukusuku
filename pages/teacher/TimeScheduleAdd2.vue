d9b1a54460e0e1084d2d4068ea50b53d927215b8
丸野 廉士郎 <st20183150@GHOST.KCSNet.LOCAL>
2021-11-12 10:37:29 +0900

    Add TimescAdd2

---
+++ pages/teacher/TimeScheduleAdd2.vue
@@ -0,0 +1,22 @@
+
+<template>
+<div>
+    <v-file-input
+    accept=".xlsx"
+    small-chips
+    truncate-length="15"
+    label="excel File input"
+  ></v-file-input>
+  <v-btn
+    color="accent"
+  >
+      登録
+  </v-btn>
+</div>
+</template>
+
+<script>
+export default {
+    //かっこよくしてください
+}
+</script>

ec08759c1f98655ecb5951566380f1560722675d
丸野 廉士郎 <st20183150@GHOST.KCSNet.LOCAL>
2021-11-12 16:32:01 +0900

    Component Home

---
+++ components/CompToDo.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="home">
+        <h1 class="mt-06"><v-icon size="1em">mdi-notebook-check-outline</v-icon>&emsp;Todoリスト</h1>
+        <v-divider></v-divider>
+        <v-list
+        class="pt-0"
+      flat
+    >
+    <div
+        v-for="task in tasks"
+        :key="task.id"
+    >
+        <v-list-item
+            :class="{ 'blue-grey lighten-4': task.done }"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="task.done"
+                @click="doneTask(task.id)"
+                color="accent"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{ 'text-decoration-line-through' : task.done }"
+              >
+                  {{ task.title }}
+                </v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-action>
+          <v-btn
+            @click.stop="deleteTask(task.id)"
+             icon>
+            <v-icon  color="primary">mdi-delete-circle</v-icon>
+          </v-btn>
+        </v-list-item-action>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+    </div>
+    </v-list>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Todo',
+    data() {
+        return {
+            newTaskTitle: '',
+            tasks: [
+                {
+                    id: 1,
+                    title: '宿題提出する',
+                    done: false
+                },
+                {
+                    id: 2,
+                    title: '参考書買う',
+                    done: false
+                },
+                {
+                    id: 3,
+                    title: 'ハガキ出す',
+                    done: false
+                },
+            ]
+        }
+    },
+    methods: {
+        doneTask(id) {
+            let task = this.tasks.filter(task => task.id === id) [0]
+            task.done = !task.done
+        },
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => task.id !== id)
+        }
+    },
+}
+</script>
---
+++ pages/student/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row class="fill-height"
+  color="primary">
+    <v-col cols="8">
+      <CompCalendar />
+    </v-col>
+    <v-col cols="4">
+      <v-col>
+        <v-card>
+          <CompEventCD />
+        </v-card>
+      </v-col>
+      <v-col>
+        <CompToDo />
+      </v-col>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  import items from '/components/timeTable.json'
+  import calendarAdd from '/components/calendarAdd.vue'
+  import CompCalendar from '/components/CompCalendar.vue'
+  import CompEventCD from '/components/CompEventCD.vue'
+  import CompToDo from '/components/CompToDo.vue'
+  export default {
+  /* 未ログイン時index.vueに遷移 */
+  /* middleware:"authenicated" */
+}
+</script>
